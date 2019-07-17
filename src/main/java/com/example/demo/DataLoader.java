681f6f675ca8a242704a6b124389cfe1ce19e0cf
togoboy8 <cedrichillah@gmail.com>
2019-07-17 12:14:16 -0400

    first commit

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    @Autowired
+    CourseRepository repository;
+
+    @Override
+    public void run(String... strings) throws Exception{
+        Course course= new Course("Astrophysics", "Neil D Tyson",
+                "Just a course on Stars", 3);
+        repository.save(course);
+
+        course=new Course("Calculus", "Carol Henley",
+                "Rate of Change of the Rate of Change", 3);
+        repository.save(course);
+
+        course=new Course("Freshman English", "Geraldine Pegram",
+                "Learn your language childre", 3);
+        repository.save(course);
+    }
+}
